7ef5a1d8fe2f4199d45380180749fd44d9d0631a
Michael Obubelebra Amachree <amachree9630@gmail.com>
2026-08-20 17:45:20 +0100

    chore(dashboard): add sv-agentation and configure Svelte inspector

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -6,6 +6,13 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'alt-x',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right'
+		}
+	},
 
 	kit: {
 		// adapter-netlify: SSR + remote functions run in Netlify Functions,
